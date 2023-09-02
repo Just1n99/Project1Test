65b00d4b0783054e496ea7c7738293e2adc5a200
Justin Yu <22000456@handong.ac.kr>
2023-09-02 16:20:56 +0900

    Test Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("22000456", "유승준");
+        System.out.println("Hello world!");
+        System.out.println(s1);
+    }
+}
